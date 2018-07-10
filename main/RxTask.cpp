322457207d917d73e4435cf7e47cd589e10a8a26
Andrew McDonald <andy@vcmobile.net>
2018-07-10 16:18:47 -0700

    Fixed error when returning HEADER type

--- main/RxTask.cpp
+++ main/RxTask.cpp
@@ -27,12 +27,15 @@ namespace {
 		//  First part is high, 2nd low
 		//  2nd part must be in range to be the low pulse
 		//  1st part must be in range to be header, zero or one pulse
-		if( item.level0 == 0
-				|| item.level1 == 1
+		ESP_LOGI(TAG, "DECODE %d:%d - %d:%d", item.level0, item.duration0, item.level1, item.duration1);
+
+		if( item.level0 == 1
+				|| item.level1 == 0
 				|| item.duration1 > (cTicksLow + cTicksMargin)
 				|| item.duration1 < (cTicksLow - cTicksMargin)
 				|| item.duration0 < (cZeroTicksHigh - cTicksMargin)
 				|| item.duration0 > (cHeaderTicksHigh + cTicksMargin)) {
+			ESP_LOGI(TAG, "EARLY INVALID!!!");
 			return INVALID;
 		}
 
@@ -49,19 +52,21 @@ namespace {
 
 		if( (item.duration0 >= cHeaderTicksHigh - cTicksMargin)
 				&& (item.duration0 <= cHeaderTicksHigh + cTicksMargin) ) {
-			return LEVEL_1;
+			return HEADER;
 		}
 
 		return INVALID;
 	}
 
-	inline rmt_item32_t *decode_message( rmt_item32_t *pCurr, rmt_item32_t *pEnd, uint8_t *data, size_t *dataSize ) {
+	inline rmt_item32_t *decode_message( rmt_item32_t *pCurr, rmt_item32_t *pEnd, uint8_t *data, int *dataSize ) {
 		int currentByte=-1;
 		int currentBit=0;
 
 		while(pCurr != pEnd) {
 			switch(decode_item(*pCurr)) {
 				case HEADER:
+					ESP_LOGI(TAG, "DECODE::HEADER" );
+
 					if(currentByte<0) {
 						currentByte=0;
 					} else {
@@ -74,6 +79,8 @@ namespace {
 
 					break;
 				case LEVEL_1:
+					ESP_LOGI(TAG, "DECODE::LEVEL_1" );
+
 					if(currentBit>=8) {
 						++currentByte;
 						currentBit=0;
@@ -84,6 +91,8 @@ namespace {
 
 					break;
 				case LEVEL_0:
+					ESP_LOGI(TAG, "DECODE::LEVEL_0" );
+
 					if(currentBit>=8) {
 						++currentByte;
 						currentBit=0;
@@ -94,10 +103,13 @@ namespace {
 
 					break;
 				default:
+					ESP_LOGI(TAG, "DECODE::UNKNOWN" );
+
 					currentBit=0;
 					currentByte=-1;
 					break;
 			}
+			++pCurr;
 		}
 
 		*dataSize = currentByte;
@@ -131,26 +143,33 @@ void RxTask::run(void *data)
     rmt_get_ringbuf_handle(m_RmtChannel, &rb);
     rmt_rx_start(m_RmtChannel, 1);
     while(rb) {
+		//ESP_LOGI(TAG, "RingBuffer Loop");
+
         size_t rx_size = 0;
         //try to receive data from ringbuffer.
         //RMT driver will push all the data it receives to its ringbuffer.
         //We just need to parse the value and return the spaces of ringbuffer.
-        rmt_item32_t* item = (rmt_item32_t*) xRingbufferReceive(rb, &rx_size, 1000);
-        if(item) {
+        void *buff = xRingbufferReceive(rb, &rx_size, 0);
+        if(NULL != buff) {
+			rmt_item32_t* item = (rmt_item32_t*)buff;
+			ESP_LOGI(TAG, "Ringbuffer received!!!");
 			rmt_item32_t *pEnd = item + (rx_size/sizeof(rmt_item32_t));
 			uint8_t data[4];
-			size_t dataSize = 4;
+			int dataSize = 4;
 
 			while(item != pEnd) {
 				dataSize = 4;
 				item = decode_message(item, pEnd, data, &dataSize );
 				if(dataSize>0) {
+					ESP_LOGI(TAG, "Recieved message size=%d", dataSize );
 					// Something was decoded
-					for(int i=0; i<dataSize; ++i) {
-						ESP_LOGI(TAG, "Recieved message Byte %d/%d = 0x%x", i, dataSize, data[i] );
-					}
+					// for(int i=0; i<dataSize; ++i) {
+					// 	ESP_LOGI(TAG, "Recieved message Byte %d/%d = 0x%x", i, dataSize, data[i] );
+					// }
 				}
 			}
+			vRingbufferReturnItem(rb, buff);
 		}
+		this->delay(500);
 	}
 }
